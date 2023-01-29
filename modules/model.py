2fe8dd7dd03118d7c6ac7f1521851b422beffba1
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-01-29 12:27:07 +0100

    Move image generation to model.py

--- modules/model.py
+++ modules/model.py
@@ -1,23 +1,20 @@
 import numpy as np
 import anvil
-import modules
-
+from light import Light
+from PIL import Image, ImageDraw
 
 class Model:
-    def __init__(self, config):
-        self.config = config
-
-        self.height
-        self.width
+    def __init__(self):
+        self.light = Light('self.model.model')
+        self.height = 0
+        self.width = 0
+        self.set_dimensions()
         self.model = np.zeros((self.width, self.height, self.width))
 
-    def set_dimensions(self, height: int, width: int):
+    # TODO: set dimension through config file
+    def set_dimensions(self):
         'height and width must be in a 2:1 ratio'
-        if height/2 == width:
-            self.height = height
-            self.width = width
-        else:
-            return
+        pass
 
     def save(self):
         """save model in file"""
@@ -36,6 +33,66 @@ class Model:
                     if self.model[layer][row][voxel] == self.config["materials"]["material"]:
                         self.model[layer][row][voxel] = self.config["materials"]["air"]
 
+    def generate_images(self):
+        sides = ['front', 'back', 'left', 'right']
+        for side in sides:
+            image = Image.new("RGB", (self.width, self.height), (0, 0, 0))
+            d = ImageDraw.Draw(image)
+
+            # array to save visible voxels
+            visible = np.zeros((self.height, self.width))
+
+            if side == 'front':
+                # iterate through map and add visible voxels to array
+                for layer in range(0, self.width):
+                    for row in range(0, self.height):
+                        for voxel in range(0, self.width):
+                            # there is no voxel set, set new one 
+                            if visible[row,voxel] == 0:
+                                visible[row,voxel] = self.model[layer,row,voxel]
+                
+            if side == 'back':
+                # iterate through map and add visible voxels to array
+                for layer in range(self.width-1,-1,-1):
+                    for row in range(0, self.height):
+                        for voxel in range(0, self.width):
+                            # there is no voxel set, set new one 
+                            if visible[row,-voxel-1] == 0:
+                                # 'mirror' voxel value, because of iteration from back
+                                visible[row,-voxel-1] = self.model[layer,row,voxel]
+
+            if side == 'left':
+                for layer in range(0, self.width):
+                    for row in range(0, self.height):
+                        for voxel in range(0, self.width):
+                            # if visible empty on this index, set to current voxel
+                            if visible[row,self.width-(layer+1)] == 0:
+                                visible[row,self.width-(layer+1)] = self.model[layer,row,voxel]
+
+            if side == 'right':
+                for layer in range(self.width-1,-1,-1):
+                    for row in range(0, self.height):
+                        for voxel in range(0, self.width):
+                            # if visible empty on this index, set to current voxel
+                            to_mirror = self.width-(layer+1)
+                            if visible[row,-to_mirror-1] == 0:
+                                visible[row,-to_mirror-1] = self.model[layer,row,voxel]
+
+            # TODO: get material ids from config file
+            # iterate through visible voxels and add them to image
+            for row in range(0, self.height):
+                for voxel in range(0, self.width):
+                    if visible[row,voxel] == 1:
+                        d.rectangle(((voxel, row), (voxel, row)), color_leaf)
+                    elif visible[row,voxel] == 2:
+                        d.rectangle(((voxel, row), (voxel, row)), color_wood)
+            # reset visible
+            visible = np.zeros((self.height, self.width))
+            
+            # save image to file
+            image.save('images/'+side+'.png')
+
+    # TODO: fix path, maybe add to config
     def model_minecraft(self, arr, path: str = "/Users/philippschuetz/Library/Application Support/minecraft/saves/Simulation/region/"):
         """
         load np array into a minecraft world
