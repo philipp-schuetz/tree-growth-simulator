d973e773324b7ddab82212ed90d3547c53e30b7c
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-01-29 12:45:11 +0100

    Add ui trigger for export to png

--- modules/app.py
+++ modules/app.py
@@ -1,15 +1,19 @@
 """Module contains the App class."""
-from ui import Ui
-from model import Model
+from .ui import Ui
+from .model import Model
 
 class App:
     """App contains ui, model and light objects and the mainloop of this application"""
     def __init__(self):
         self.ui = Ui()
         self.model = Model()
-        self.model.set_dimensions(500, 250)
+        # self.model.set_dimensions(500, 250)
 
     def run(self):
         """run main event loop"""
         while self.ui.window_status == 1:
-            self.ui.handle_window()
+            match self.ui.handle_window():
+                case 'start':
+                    pass
+                case 'export':
+                    self.model.generate_images()
