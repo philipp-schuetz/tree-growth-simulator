4586fb322a13a29c8cd2a1c7ae7c0c40a975c80c
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-02-04 21:39:23 +0100

    Edit light calc

--- modules/light.py
+++ modules/light.py
@@ -3,7 +3,7 @@ import modules.config as config
 # TODO: finish light calculation
 
 class Light:
-    def __init__(self):
+    def __init__(self, model):
         self.config = config.Config()
         # {0: 100, 1: 0, 2: 50, 3: 0}
         self.translucency = {}
@@ -35,12 +35,14 @@ class Light:
         """set inital lightlevel from ui"""
         self.lightlevel = light
 
-    def calculate(self, baselight:int):
+    def calculate(self, model):
         """calculates the lightvalue for each voxel \n
         only one lightvalue is calculated per voxel\n
         the value calculated refers to the side most close to the models edge"""
 
-        sides = ['front','back','left','right','top']
+        # sides = ['front','back','left','right','top']
+        sides = {'front':100,'back':100,'left':100,'right':100,'top':100}
+
         for side in sides:
             if side == 'front':
                 # iterate through map and add visible voxels to array
@@ -50,9 +52,14 @@ class Light:
                             # set outer layer of voxels to initital light level
                             if layer == 0:
                                 self.lightarray[layer,row,voxel] = self.lightlevel
-                            # get light value of last voxel, reduce it and add to lightarray
-                            self.lightarray[layer,row,voxel]
-                
+                            else:
+                                # calculate new light value and add it to light array
+
+                                light_back = self.lightarray[layer-1,row,voxel]
+                                translucency_back = self.translucency[model[layer-1,row,voxel]]
+
+                                self.lightarray[layer,row,voxel] += light_back*(translucency_back/100)
+
             if side == 'back':
                 # iterate through map and add visible voxels to array
                 for layer in range(self.width-1,-1,-1):
