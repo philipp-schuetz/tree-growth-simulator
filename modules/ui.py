d9a9ef1ec8db54f60f27f169063abc29865f421d
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-02-05 12:15:00 +0100

    Fix pylint errors in ui

--- modules/ui.py
+++ modules/ui.py
@@ -16,20 +16,38 @@ class Ui:
         # set ui theme
         sg.theme('DarkBlue')
 
-        # define ui layout 
+        # define ui layout
         self.layout = [
             [sg.Frame(title='Modifiers', layout=[
-                [sg.Text('Light in %'), sg.Slider(range=(0,200), default_value=100, key='light', orientation='horizontal')],
-                [sg.Checkbox('front', default=True, key='front'),sg.Checkbox('back', default=True, key='back'),sg.Checkbox('left', default=True, key='left'),sg.Checkbox('right', default=True, key='right'),sg.Checkbox('top', default=True, key='top')],
-                [sg.Text('Water in %'), sg.Slider(range=(0,200), default_value=100, key='water', orientation='horizontal')],
-                [sg.Text('Temperature in %'), sg.Slider(range=(0,200), default_value=100, key='temperature', orientation='horizontal')],
-                [sg.Text('Nutrients in %'), sg.Slider(range=(0,200), default_value=100, key='nutrients', orientation='horizontal')]
+                [
+                    sg.Text('Light in %'),
+                    sg.Slider(range=(0,200), default_value=100, key='light', orientation='horizontal')
+                ],
+                [
+                    sg.Checkbox('front', default=True, key='front'),
+                    sg.Checkbox('back', default=True, key='back'),
+                    sg.Checkbox('left', default=True, key='left'),
+                    sg.Checkbox('right', default=True, key='right'),
+                    sg.Checkbox('top', default=True, key='top')
+                ],
+                [
+                    sg.Text('Water in %'),
+                    sg.Slider(range=(0,200), default_value=100, key='water', orientation='horizontal')
+                ],
+                [
+                    sg.Text('Temperature in %'),
+                    sg.Slider(range=(0,200), default_value=100, key='temperature', orientation='horizontal')
+                ],
+                [
+                    sg.Text('Nutrients in %'),
+                    sg.Slider(range=(0,200), default_value=100, key='nutrients', orientation='horizontal')
+                ]
             ]), sg.Frame(title='Buttons', layout=[
                 [sg.Button('Start')],
                 [sg.Button('Export to PNGs')]
             ])]
         ]
-        
+
         # create window
         self.window = sg.Window('tree-growth-simulator', self.layout)
         self.window_status = 1
@@ -37,7 +55,7 @@ class Ui:
     def handle_window(self):
         """handle window events and values in the mainloop"""
         event, values = self.window.read()  # type: ignore
-        
+
         # check for events
         match event:
             case 'Start':
@@ -47,8 +65,13 @@ class Ui:
                 self.value_temperature = values['temperature']
                 self.value_nutrients = values['nutrients']
 
-                self.values_light_sides = [values['front'], values['back'],values['left'], values['right'], values['top']]
-                
+                self.values_light_sides = [
+                    values['front'],
+                    values['back'],
+                    values['left'],
+                    values['right'],
+                    values['top']
+                ]
                 return 'start'
             case 'Export to PNGs':
                 sg.popup('Please be patient while the images are being generated.', title='Export to PNGs')
@@ -57,20 +80,26 @@ class Ui:
                 self.close_window()
 
     def close_window(self) -> None:
+        """close the currently active window"""
         self.window.close()
         self.window_status = 0
 
     def get_light(self) -> int:
+        """get the light value"""
         return self.value_light
-    
+
     def get_light_sides(self) -> list[bool]:
+        """get the sides for which light calculation is activated"""
         return self.values_light_sides
 
     def get_water(self) -> int:
+        """get the water value"""
         return self.value_water
 
     def get_temperature(self) -> int:
+        """get the temperature value"""
         return self.value_temperature
 
     def get_nutrients(self) -> int:
+        """get the nutrients value"""
         return self.value_nutrients
