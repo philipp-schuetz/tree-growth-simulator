710af8e2d6232b46643622a52188c0fbb18a1ab4
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-03-18 11:19:21 +0100

    Change action types, Add radius for voxel placement

--- action_interpreter.py
+++ action_interpreter.py
@@ -1,5 +1,4 @@
 import numpy as np
-import mcmodel
 import string
 
 # cut the forward move length in half every time
@@ -10,13 +9,15 @@ class Actions():
         self.material = 2
         self.base_width = 1
         self.autoplace = True # execute place function after every move if set to true
+        # base radius for placing
+        self.radius = 0
 
         self.actions = ['u3','f','b2','f','l','r2','re'] # actions to execute on run
         self.position = [] # current position
         self.positions = [] # saved positions
 
         # set first positions
-        self.start_pos = [int(width/2), height-1, int(width/2)]
+        self.start_pos = [int(width/2+0.5), height-1, int(width/2+0.5)]
         self.position = self.start_pos
         self.positions = [self.start_pos]
 
@@ -24,36 +25,16 @@ class Actions():
 
 
     def place(self):
-        'set current voxel to specified material'
-        # TODO: place multiple voxels with one place method iteration
-        for i in range(0, self.base_width*self.base_width):
+        """set current voxel(s) to specified material"""
+        if self.radius > 0:
+            # row stays the same, radius defines the with of the tree
+            for layer in range(self.position[0]-self.radius, self.position[0]+self.radius):
+                for voxel in range(self.position[2]-self.radius, self.position[2]+self.radius):
+                    self.model[layer,self.position[1],voxel] = self.material
+        elif self.radius == 0:
             self.model[self.position[0],self.position[1],self.position[2]] = self.material
-    def forward(self):
-        'move forward one voxel'
-        self.position[0] = self.position[0]+1
-    def backward(self):
-        'move backward one voxel'
-        self.position[0] = self.position[0]-1
-    def left(self):
-        'move left one voxel'
-        self.position[2] = self.position[2]-1  
-    def right(self):
-        'move right one voxel'
-        self.position[2] = self.position[2]+1
-    def up(self):
-        'move up one voxel'
-        self.position[1] = self.position[1]-1
-    def down(self):
-        self.position[1] = self.position[1]+1
-    def save(self):
-        'save current position'
-        self.positions.append(self.position)
-    def set(self):
-        'set position to last saved position'
-        self.position = self.positions[-1]
-    def reset(self):
-        'set position to start position'
-        self.position = self.start_pos
+        else:
+            raise ValueError("radius for voxel placement can't be nagative")
 
     def execute(self):
         action_type = ''
@@ -71,33 +52,27 @@ class Actions():
                 match action_type:
                     case 'p':
                         self.place()
-                    case 'f':
-                        self.forward()
-                    case 'b':
-                        self.backward()
-                    case 'l':
-                        self.left()
-                    case 'r':
-                        self.right()
-                    case 'u':
-                        self.up()
-                    case 'd':
-                        self.down()
-                    case 'sa':
-                        self.save()
-                    case 'se':
-                        self.set()
-                    case 're':
-                        self.reset()
-                
+                    case 'l': # toward negative layer index
+                        self.position[0] = self.position[0]-1
+                    case 'L': # toward positive layer index
+                        self.position[0] = self.position[0]+1
+                    case 'v': # toward negative voxel index
+                        self.position[2] = self.position[2]-1
+                    case 'V': # toward positive voxel index
+                        self.position[2] = self.position[2]+1
+                    case 'c': # center Downward
+                        self.position[1] = self.position[1]+1
+                    case 'C': # center Upward
+                        self.position[1] = self.position[1]-1
+                    case 'r': # radius smaller
+                        if self.radius != 0:
+                            self.radius -= 1
+                    case 'R': # radius larger
+                        self.radius += 1
+                    case '[': # save position
+                        self.positions.append(self.position)
+                    case ']': # get saved position
+                        self.position = self.positions.pop(-1)
+
                 if self.autoplace and action != 'p':
                     self.place()
-                i += 1
-
-    def run(self):
-        self.execute()
-        mcmodel.model_minecraft(self.model)
-
-actions = Actions(8, 4)
-
-actions.run()
