8939ca597c222802afbde2ba3adbf36c632a7889
Philipp Schütz <82471105+philipp-schuetz@users.noreply.github.com>
2023-06-18 21:22:17 +0200

    Remove TODOs from main.py

--- src/main.py
+++ src/main.py
@@ -4,13 +4,3 @@ app = App()
 
 if __name__ == "__main__":
     app.run()
-
-
-# TODO recalculate light levels only when needed, does tree need light every step?
-
-# TODO add process logging
-# TODO enable the user to get images with and without leafs at the same time
-# leaf generation
-# from bottom to top?
-# minimum lightlevel for leafes
-# place next to wood, configure direct or diagonal in config file
